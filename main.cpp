5e2fee7a413411e7d4404a32bc483f1a424f4cdb
orosmatthew <orosmatthew@pm.me>
2023-11-30 16:02:17 -0500

    Use vector and loop to 100k

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <list>
 #include <map>
 #include <random>
 #include <unordered_set>
 
-void shuffle(std::list<int>& list)
+void shuffle(std::vector<int>& arr)
 {
-    for (int i = static_cast<int>(list.size()) - 1; i > 0; --i) {
+    for (int i = static_cast<int>(arr.size()) - 1; i > 0; --i) {
         const int j = std::rand() % (i + 1); // NOLINT(*-msc50-cpp)
-        std::iter_swap(std::next(list.begin(), i), std::next(list.begin(), j));
+        std::swap(arr[i], arr[j]);
     }
 }
 
-bool check_distinct(const std::list<int>& list)
+bool check_distinct(const std::vector<int>& list)
 {
     std::unordered_set<int> set;
     bool distinct = true;
@@ -31,15 +30,15 @@ int main()
 {
     std::srand(69); // NOLINT(*-msc51-cpp)
 
-    std::list<int> list;
+    std::vector<int> arr;
     std::map<int, double> times;
-    for (int i = 0; i < 10000; ++i) {
+    for (int i = 0; i < 100000; ++i) {
         std::cout << "n = " << i << "\n";
-        list.push_back(i);
-        shuffle(list);
+        arr.push_back(i);
+        shuffle(arr);
 
         auto start = std::chrono::high_resolution_clock::now();
-        check_distinct(list);
+        check_distinct(arr);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double, std::milli> duration = end - start;
